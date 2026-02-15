4d46310437c7be308e1473dbf91648c48fa82b2b
Arpitsoniofficial0214 <154724745+Arpitsoniofficial0214@users.noreply.github.com>
2026-02-16 00:43:03 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+public class Pattern25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner sc=new Scanner(System.in);
+				int n=sc.nextInt();
+				int star=1;
+				int space=n-1;
+				int row=1;
+				while(row<=n) {
+					// space
+					int i=1;
+					while(i<=space) {
+						System.out.print(" ");
+						i++;
+					}
+					//star
+					int j=1;
+					while(j<=star) {
+						System.out.print(row+"");
+						j++;
+					}
+					//next row prep
+					System.out.println();
+					row++;
+					star+=2;
+					space--;
+				}
+			}
+
+		}
+
---
+++ pattern26.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+public class pattern26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int star=1;
+int space=n-1;
+int row=1;
+while(row<=n) {
+	//space
+	int i=1;
+	while(i<=space){
+		System.out.print(" ");
+		i++;
+	}
+	//star
+	int j=1;
+	int val=1;
+	while(j<=star){
+		System.out.print(val+"");
+		val++;
+		j++;
+	}
+	//next Row Prep
+	System.out.println();
+	row++;
+	star+=2;
+	space--;
+}
+	}
+
+}
